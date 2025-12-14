e46f3aeedf387dd5c3541a2e6f1116b36edaa0f1
SajimRZ <the8bitcrow@gmail.com>
2025-12-14 22:25:06 +0600

    template setup

---
+++ game.py
@@ -0,0 +1,168 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+# Camera-related variables
+camera_pos = (0,500,500)
+target_pos = (0,0,0)
+
+fovY = 120  # Field of view
+GRID_LENGTH = 600  # Length of grid lines
+rand_var = 423
+
+
+def draw_text(x, y, text, font=GLUT_BITMAP_HELVETICA_18):
+    glColor3f(1,1,1)
+    glMatrixMode(GL_PROJECTION)
+    glPushMatrix()
+    glLoadIdentity()
+    
+    # Set up an orthographic projection that matches window coordinates
+    gluOrtho2D(0, 1000, 0, 800)  # left, right, bottom, top
+
+    
+    glMatrixMode(GL_MODELVIEW)
+    glPushMatrix()
+    glLoadIdentity()
+    
+    # Draw text at (x, y) in screen coordinates
+    glRasterPos2f(x, y)
+    for ch in text:
+        glutBitmapCharacter(font, ord(ch))
+    
+    # Restore original projection and modelview matrices
+    glPopMatrix()
+    glMatrixMode(GL_PROJECTION)
+    glPopMatrix()
+    glMatrixMode(GL_MODELVIEW)
+
+
+
+def keyboardListener(key, x, y):
+    """
+    Handles keyboard inputs for player movement, gun rotation, camera updates, and cheat mode toggles.
+    """
+    # # Move forward (W key)
+    # if key == b'w':  
+
+    # # Move backward (S key)
+    # if key == b's':
+
+    # # Rotate gun left (A key)
+    # if key == b'a':
+
+    # # Rotate gun right (D key)
+    # if key == b'd':
+
+    # # Toggle cheat mode (C key)
+    # if key == b'c':
+
+    # # Toggle cheat vision (V key)
+    # if key == b'v':
+
+    # # Reset the game if R key is pressed
+    # if key == b'r':
+
+
+def specialKeyListener(key, x, y):
+    """
+    Handles special key inputs (arrow keys) for adjusting the camera angle and height.
+    """
+    global camera_pos
+    x, y, z = camera_pos
+    # Move camera up (UP arrow key)
+    # if key == GLUT_KEY_UP:
+
+    # # Move camera down (DOWN arrow key)
+    # if key == GLUT_KEY_DOWN:
+
+    # moving camera left (LEFT arrow key)
+    # if key == GLUT_KEY_LEFT:
+    #     x -= 1  # Small angle decrement for smooth movement
+
+    # # moving camera right (RIGHT arrow key)
+    # if key == GLUT_KEY_RIGHT:
+    #     x += 1  # Small angle increment for smooth movement
+
+    camera_pos = (x, y, z)
+
+
+def mouseListener(button, state, x, y):
+    """
+    Handles mouse inputs for firing bullets (left click) and toggling camera mode (right click).
+    """
+        # # Left mouse button fires a bullet
+        # if button == GLUT_LEFT_BUTTON and state == GLUT_DOWN:
+
+        # # Right mouse button toggles camera tracking mode
+        # if button == GLUT_RIGHT_BUTTON and state == GLUT_DOWN:
+
+
+def setupCamera():
+    global camera_pos, target_pos
+    """
+    Configures the camera's projection and view settings.
+    Uses a perspective projection and positions the camera to look at the target.
+    """
+    glMatrixMode(GL_PROJECTION)  # Switch to projection matrix mode
+    glLoadIdentity()  # Reset the projection matrix
+    # Set up a perspective projection (field of view, aspect ratio, near clip, far clip)
+    gluPerspective(fovY, 1.25, 0.1, 1500) # Think why aspect ration is 1.25?
+    glMatrixMode(GL_MODELVIEW)  # Switch to model-view matrix mode
+    glLoadIdentity()  # Reset the model-view matrix
+
+    # Extract camera position and look-at target
+    x, y, z = camera_pos
+    a, b, c = target_pos
+    # Position the camera and set its orientation
+    gluLookAt(x, y, z,  # Camera position
+              a, b, c,  # Look-at target
+              0, 0, 1)  # Up vector (z-axis)
+
+
+def idle():
+    """
+    Idle function that runs continuously:
+    - Triggers screen redraw for real-time updates.
+    """
+    # Ensure the screen updates with the latest changes
+    glutPostRedisplay()
+
+
+def showScreen():
+    """
+    Display function to render the game scene:
+    - Clears the screen and sets up the camera.
+    - Draws everything of the screen
+    """
+    # Clear color and depth buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()  # Reset modelview matrix
+    glViewport(0, 0, 1000, 800)  # Set viewport size
+
+    setupCamera()  # Configure camera perspective
+
+    #add models here
+
+    # Swap buffers for smooth rendering (double buffering)
+    glutSwapBuffers()
+
+
+# Main function to set up OpenGL window and loop
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)  # Double buffering, RGB color, depth test
+    glutInitWindowSize(1000, 800)  # Window size
+    glutInitWindowPosition(0, 0)  # Window position
+    wind = glutCreateWindow(b"3D OpenGL Intro")  # Create the window
+
+    glutDisplayFunc(showScreen)  # Register display function
+    glutKeyboardFunc(keyboardListener)  # Register keyboard listener
+    glutSpecialFunc(specialKeyListener)
+    glutMouseFunc(mouseListener)
+    glutIdleFunc(idle)  # Register the idle function to move the bullet automatically
+
+    glutMainLoop()  # Enter the GLUT main loop
+
+if __name__ == "__main__":
+    main()
